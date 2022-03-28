5da9abac366f4b1f55b59c563d1cfc4fac333b37
Jexblue <91774746+Jexblue@users.noreply.github.com>
2022-03-28 16:18:05 +0300

    test

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + 'm1-P9-Ekaly'));
+app.use(express.static(__dirname + 'ekaly-app'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/m1-P9-Ekaly/index.html'));});
+'/dist/ekaly-app/index.html'));});
 app.listen(process.env.PORT || 8080);
