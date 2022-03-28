3b6fa9073c850c038ec07336e936f368459cbbb3
Jexblue <91774746+Jexblue@users.noreply.github.com>
2022-03-28 15:58:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + 'M1-P9-Ekaly'));
+app.use(express.static(__dirname + 'm1-P9-Ekaly'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/<app-name>/index.html'));});
