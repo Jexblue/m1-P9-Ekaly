f816421309b8d33e4975b78ea108ed93ab9406e7
Jexblue <91774746+Jexblue@users.noreply.github.com>
2022-03-28 16:41:41 +0300

    test

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ekaly-app'),
+      dir: require('path').join(__dirname, './coverage/mon-application-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
