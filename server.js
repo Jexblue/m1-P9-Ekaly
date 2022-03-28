f97f0b3785cf1d382471f422512ae6e69d51f230
Jexblue <91774746+Jexblue@users.noreply.github.com>
2022-03-28 16:10:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + 'm1-P9-Ekaly'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+'/dist/m1-P9-Ekaly/index.html'));});
 app.listen(process.env.PORT || 8080);
