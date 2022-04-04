ef68a05eaa34e36dcb2eafdef1fb639edfe8ae7e
Jexblue <>
2022-04-04 16:42:47 +0300

    pages login, register, icon menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
-
+  {path : 'login', component: LoginComponent},
+  {path : 'register', component: RegisterComponent}
 
 
 ];
